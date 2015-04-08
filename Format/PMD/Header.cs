6416a039529ed48b42014f913fb1e82ccbfffeec
赤木詠滋 <s1223077@cce.kanagawa-it.ac.jp>
2015-04-08 13:16:19 +0900

    PMXフォルダ作成、およびPMDファイルにメモ書き追加

--- Format/PMD/Header.cs
+++ Format/PMD/Header.cs
@@ -10,15 +10,19 @@ namespace MMD
             {
                 public string magic;
                 public float version;
+                //public byte lengthbyte;//バイト列を格納するための変数
                 public string modelName;
                 public string comment;
 
                 public override void Read(System.IO.BinaryReader r)
                 {
-                    magic = ReadString(r, 3);
-                    version = ReadFloat(r);
-                    modelName = ReadString(r, 20);
-                    comment = ReadString(r, 256);
+                    magic = ReadString(r, 3);//マジックナンバー、PMXの宣言、PMDと同じでいいか？
+                    version = ReadFloat(r);//PMXのVer、たぶんここは一緒でいい気がする
+
+                    //lengthbyte = Readbyte(r);//便宜的に追加的な、どこで呼び出せるかとかは考える
+
+                    modelName = ReadString(r, 20);//モデル情報、モデルの名前、英語にも対応可能・・・のはず
+                    comment = ReadString(r, 256);//モデルのコメント、こちらも英語に対応可能
                 }
             }
         }
